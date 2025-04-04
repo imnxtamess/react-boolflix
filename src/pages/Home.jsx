d87c86f1c2bee8532a9156749b08dc90d00879e1
imnxtamess <davide.morisco.work@gmail.com>
2025-04-04 17:26:38 +0200

    Enhance Home component to display movie titles, original titles, country flags, and star ratings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,18 @@ export default function Home() {
     useMoviesContext();
   console.log(moviesPlusTv);
 
+  function scoreToStars(score) {
+    const star = "\u{2B50}";
+    const halfScore = parseFloat(score / 2).toFixed(1);
+    let starArr = [];
+    for (let index = 0; index <= halfScore; index++) {
+      starArr.push(star);
+    }
+
+    starArr.join("");
+    return starArr;
+  }
+
   return (
     <>
       <div className="container">
@@ -43,23 +55,48 @@ export default function Home() {
           </button>
         </div>
 
-        <div className="row">
+        <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 g-3 mt-5 text-center">
           {moviesPlusTv.map((movie) => (
             <div key={movie.id} className="col">
               <div className="card">
-                <h1>{movie.overview}</h1>
-                <h1>{movie.title}</h1>
-                <h3>{movie.original_title}</h3>
-                <h5>
-                  <ReactCountryFlag
-                    countryCode={"en" ? "gb" : movie.original_language}
-                    cdnUrl={`https://cdn.jsdelivr.net/gh/lipis/flag-icons/flags/4x3/`}
-                    cdnSuffix="svg"
-                    title={movie.original_language}
-                    svg
-                  />
-                </h5>
-                <span>{movie.vote_average}</span>
+                <ul className="list-unstyled">
+                  <li>
+                    <h1>{movie.title ? movie.title : movie.name}</h1>
+                  </li>
+                  <li>
+                    <h3>
+                      {movie.original_title
+                        ? movie.original_title
+                        : movie.original_name}
+                    </h3>
+                  </li>
+                  <li>
+                    <h5>
+                      <ReactCountryFlag
+                        countryCode={"en" ? "gb" : movie.original_language}
+                        cdnUrl={`https://cdn.jsdelivr.net/gh/lipis/flag-icons/flags/4x3/`}
+                        cdnSuffix="svg"
+                        title={movie.original_language}
+                        svg
+                      />
+                    </h5>
+                  </li>
+                  <li>
+                    <span>{scoreToStars(movie.vote_average)}</span>
+                  </li>
+                  <li>
+                    <img
+                      width="154"
+                      className="img-fuid"
+                      src={
+                        movie.poster_path
+                          ? `https://image.tmdb.org/t/p/w154${movie.poster_path}`
+                          : `https://www.svgrepo.com/show/508699/landscape-placeholder.svg`
+                      }
+                      alt=""
+                    />
+                  </li>
+                </ul>
               </div>
             </div>
           ))}
